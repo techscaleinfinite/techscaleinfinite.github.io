57665119d2d655e411bfa4737b5f2d9ff780b6be
techscaleinfinite <121960860+techscaleinfinite@users.noreply.github.com>
2025-06-24 15:30:00 +0530

    Play store apps listed from sql lite

--- src/pages/playstore/index.jsx
+++ src/pages/playstore/index.jsx
@@ -38,6 +38,8 @@ const AppItem = ({ image, title, desc, rating }) => (
 
 const PlaystoreLandingPage = () => {
   const [apps, setApps] = useState([]);
+  const [category, setCategory] = useState([]);
+
 
   useEffect(() => {
     (async () => {
@@ -45,21 +47,35 @@ const PlaystoreLandingPage = () => {
         locateFile: file => `/sql-wasm.wasm`,
       });
 
+        // Load category.sqlite
+    const catRes = await fetch('/category.sqlite');
+    const catBuffer = await catRes.arrayBuffer();
+    const catDB = new SQL.Database(new Uint8Array(catBuffer));
+
+    const catResult = catDB.exec("SELECT * FROM category");
+    const categories = catResult[0].values.map(row => ({
+      id: row[0],
+      name: row[1],
+      slug: row[2],
+    }));
+
       const response = await fetch('/apps.sqlite');
       const buffer = await response.arrayBuffer();
       const db = new SQL.Database(new Uint8Array(buffer));
-            const result = db.exec("SELECT * FROM apps");
+      const result = db.exec("SELECT * FROM apps");
 
 
       // Convert result[0] to array of objects
       const loadedApps = result[0].values.map(row => ({
-        image: row[1],
+        image: row[3],
         title: row[2],
-        desc: row[3],
-        rating: row[4],
+        desc: row[1],
+        rating: row[5],
+        category:row[1],
       }));
 
       setApps(loadedApps);
+      setCategory(categories);
        })();
        }, []);
   /*const apps = [
@@ -189,166 +205,97 @@ const products = [
         {apps.slice(8, 12).map((app, idx) => <AppItem key={idx + 8} {...app} />)}
       </div>
     </div>
-    {/* Open source Slider  */}
-    <section className="new-arrivals-section position-relative">
-      <div className="container">
-        <div className="row mb-0 mt-2">
-          <div className="col-12">
-            <div className="section-title text-start">
-              <h2>Open Source Products</h2>
+    <>
+    {category.map((cat, index) => {
+      // 🔍 Filter products for this category
+        const filteredProducts = apps.filter(app => app.category.toLowerCase() === cat.slug.toLowerCase());
+      return (
+        <section key={index} className="new-arrivals-section position-relative">
+          <div className="container">
+            <div className="row mb-0 mt-2">
+              <div className="col-12">
+                <div className="section-title text-start">
+                  <h2>{cat.name}</h2>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        {/* Custom Arrows - outside Swiper */}
-        <div className="swiper-button-prev custom-swiper-button">
-          <i className="fa fa-angle-left" ></i>
-        </div>
-        <div className="swiper-button-next custom-swiper-button">
-          <i className="fa fa-angle-right"></i>
-        </div>
+            {/* Custom Arrows - outside Swiper */}
+            <div className="swiper-button-prev custom-swiper-button">
+              <i className="fa fa-angle-left"></i>
+            </div>
+            <div className="swiper-button-next custom-swiper-button">
+              <i className="fa fa-angle-right"></i>
+            </div>
 
-        <Swiper
-          modules={[Navigation]}
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }}
-          spaceBetween={10}
-          slidesPerView={1}
-          loop={true}
-          speed={1000}
-          breakpoints={{
-            576: { slidesPerView: 2 },
-            768: { slidesPerView: 3 },
-            992: { slidesPerView: 4 },
-            1200: { slidesPerView: 5 },
-          }}
-          centeredSlides={false}
-          grabCursor={true}
-        >
-          {products.map((product, index) => (
-            <SwiperSlide key={index}>
-              <div className="product-layout d-flex justify-content-center">
-                <div className="product-thumb">
-                  <div className="product-inner">
-                    <div className="product-image">
-                      <a href="#">
-                        <img src={product.img} alt={product.name} title={product.name} />
-                      </a>
-                    </div>
-                    <div className="product-caption">
-                      <div className="product-meta d-flex flex-column align-items-center">
-                        <p className="product-price mb-2">
-                          <span className="price-new">{product.name}</span>
-                        </p>
-                        <div className="product-manufacturer mb-1">
-                          <a href="#">{product.category}</a>
+            <Swiper
+              modules={[Navigation]}
+              navigation={{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              }}
+              spaceBetween={10}
+              slidesPerView={1}
+              loop={true}
+              speed={1000}
+              breakpoints={{
+                576: { slidesPerView: 2 },
+                768: { slidesPerView: 3 },
+                992: { slidesPerView: 4 },
+                1200: { slidesPerView: 5 },
+              }}
+              centeredSlides={false}
+              grabCursor={true}
+            >
+              {filteredProducts.map((product, pIndex) => (
+                <SwiperSlide key={pIndex}>
+                  <div className="product-layout d-flex justify-content-center">
+                    <div className="product-thumb">
+                      <div className="product-inner">
+                        <div className="product-image">
+                          <a href="#">
+                            <img
+                              src={product.image}
+                              alt={product.name}
+                              title={product.name}
+                            />
+                          </a>
                         </div>
-                        <div className="product-ratings">
-                          <ul className="rating d-flex justify-content-center list-unstyled mb-0">
-                            {[...Array(5)].map((_, i) => (
-                              <li key={i}>
-                                <i
-                                  className={`ion ion-md-star-outline${
-                                    i >= product.rating ? ' disabled' : ''
-                                  }`}
-                                ></i>
-                              </li>
-                            ))}
-                          </ul>
+                        <div className="product-caption">
+                          <div className="product-meta d-flex flex-column align-items-center">
+                            <p className="product-price mb-2">
+                              <span className="price-new">{product.title}</span>
+                            </p>
+                            <div className="product-manufacturer mb-1">
+                              <a href="#">{product.category}</a>
+                            </div>
+                            <div className="product-ratings">
+                              <ul className="rating d-flex justify-content-center list-unstyled mb-0">
+                                {[...Array(5)].map((_, i) => (
+                                  <li key={i}>
+                                    <i
+                                      className={`ion ion-md-star-outline${
+                                        i >= product.rating ? ' disabled' : ''
+                                      }`}
+                                    ></i>
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </section>
-    {/* Operating System Products */}
-    <section className="new-arrivals-section position-relative">
-      <div className="container">
-        <div className="row mb-0 mt-2">
-          <div className="col-12">
-            <div className="section-title text-start">
-              <h2>Operating System</h2>
-            </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
-        </div>
-
-        {/* Custom Arrows - outside Swiper */}
-        <div className="swiper-button-prev custom-swiper-button">
-          <i className="fa fa-angle-left" ></i>
-        </div>
-        <div className="swiper-button-next custom-swiper-button">
-          <i className="fa fa-angle-right"></i>
-        </div>
-
-        <Swiper
-          modules={[Navigation]}
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }}
-          spaceBetween={10}
-          slidesPerView={1}
-          loop={true}
-          speed={1000}
-          breakpoints={{
-            576: { slidesPerView: 2 },
-            768: { slidesPerView: 3 },
-            992: { slidesPerView: 4 },
-            1200: { slidesPerView: 5 },
-          }}
-          centeredSlides={false}
-          grabCursor={true}
-        >
-          {products.map((product, index) => (
-            <SwiperSlide key={index}>
-              <div className="product-layout d-flex justify-content-center">
-                <div className="product-thumb">
-                  <div className="product-inner">
-                    <div className="product-image">
-                      <a href="#">
-                        <img src={product.img} alt={product.name} title={product.name} />
-                      </a>
-                    </div>
-                    <div className="product-caption">
-                      <div className="product-meta d-flex flex-column align-items-center">
-                        <p className="product-price mb-2">
-                          <span className="price-new">{product.name}</span>
-                        </p>
-                        <div className="product-manufacturer mb-1">
-                          <a href="#">{product.category}</a>
-                        </div>
-                        <div className="product-ratings">
-                          <ul className="rating d-flex justify-content-center list-unstyled mb-0">
-                            {[...Array(5)].map((_, i) => (
-                              <li key={i}>
-                                <i
-                                  className={`ion ion-md-star-outline${
-                                    i >= product.rating ? ' disabled' : ''
-                                  }`}
-                                ></i>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </section>
-
-    </Layout>
+        </section>
+      );
+    })}
+  </>
+       </Layout>
   );
 };
 
