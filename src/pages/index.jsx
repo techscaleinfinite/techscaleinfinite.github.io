75a7a8cdc70f784adf83cbdb120e7d00babcdbeb
Sundar <infotosundar@gmail.com>
2025-07-03 12:06:04 +0530

    Page Test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import $ from 'jquery';
 
-
  const settings = {
     centerMode: true,          // Equivalent to `center: true`
     centerPadding: '0px',      // No padding for centered slide
